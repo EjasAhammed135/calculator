0da1dd4295a0d1c570494ab240ca96cb97262fe6
EjasAhammed135 <ejasahammed135@gmail.com>
2022-05-06 21:15:15 +0530

    first commit

---
+++ simplecalc.js
@@ -0,0 +1,9 @@
+function display(value){
+	document.getElementById('result').value+=value;
+}
+function equals(){
+	document.getElementById('result').value=eval(document.getElementById('result').value);
+}
+function clearscreen(){
+	document.getElementById('result').value=" ";
+}
